ba16fdd6ac78e5733c0163e3f5ebeb80932a4562
Elie Ishimwe <elieish@gmail.com>
2015-01-10 15:19:39 +0200

    Fixes currency converter bug

--- public/ajax.php
+++ public/ajax.php
@@ -3,7 +3,7 @@
 $amount = $_GET['amount'];
 $source_currency = $_GET['source_currency'];
 $target_currency = $_GET['target_currency'];
-$url = 'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s='. $source_currency . $target_currency .'=X';
+$url = 'http://download.finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s='. $source_currency . $target_currency .'=X';
 $handle = @fopen($url, 'r');
 
 if ($handle) {
