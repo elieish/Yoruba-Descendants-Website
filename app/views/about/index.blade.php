97c854a6ce6eb2155538520237014fa3f19d5566
Elie Ishimwe <elieish@gmail.com>
2015-01-10 16:46:00 +0200

    *Add about us page

--- app/views/about/index.blade.php
+++ app/views/about/index.blade.php
@@ -1,23 +1,78 @@
 @extends('layouts.default')
 @section('content')
 
+<!--=== Breadcrumbs ===-->
+<div class="row-fluid breadcrumbs margin-bottom-40">
+    <div class="container">
+        <h1 class="pull-left">About Us</h1>
+        <ul class="pull-right breadcrumb">
+            <li><a href="/">Home</a> <span class="divider">/</span></li>
+            <li class="active">About Us</li>
+        </ul>
+    </div><!--/container-->
+</div><!--/breadcrumbs-->
+<!--=== End Breadcrumbs ===-->
 
 <!--=== Content Part ===-->
 <div class="container">
-    <!-- Coming Soon Content -->
-    <div class="row-fluid">
-        <div class="span12">
-            <h1>Coming Soon!</h1>
+    <div class="row-fluid margin-bottom-30">
+        <div class="span6">
+            <p>The first obvious answer to this question is the Yoruba are a nationality,
+            numbering about 40 million, the majority of whom live in the South Western part of
+            the state of Nigeria in West Africa. Obvious as this answer is, it is not wholly explanatory,
+            and certainly, it is not without its own controversy. First, regarding its explanatory status.
+            One has to add, that the Yoruba are people, that speak a common language,
+            Yoruba, which belongs to the Kwa group of the Niger-Congo linguistic family,
+            and it has about 12 dialects; that they are a well urbanized group with genius
+            in arts as symbolized in the famous "Ife Bronzes"; that Yoruba people are also found in Togo,
+            Benin Republic and in other parts of the world, including Brazil,
+            Cuba, Trinidad, and the United States. Second, regarding its controversial status,
+            one has to confront the question what makes the Yoruba a nationality, or a nation,
+            not a tribe or clan, and how does one then mark a distinction between
+            Yorubaland and Nigeria. To this last question, there is no better answer than
+            the one provided by Obafemi Awolowo in 1947, to which a later section of
+            this presentation will return. For now, it is necessary to answer the question:
+            "Who are the Yoruba?" by focusing on some critical moments in Yoruba history and thought.
+            Address these and other issues by focusing on some critical moments in Yoruba</p>
         </div>
-    </div>
+        <div class="span6">
+            <div id="myCarousel" class="carousel slide">
+                <div class="carousel-inner">
+                  <div class="item active">
+                    <img src="assets/img/carousel/5.jpg" alt="" />
+                     <div class="carousel-caption">
+                      <h4></h4>
+                      <p></p>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="assets/img/carousel/4.jpg" alt="" />
+                    <div class="carousel-caption">
+                      <h4></h4>
+                      <p></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-arrow">
+                    <a data-slide="prev" href="#myCarousel" class="left carousel-control"><i class="icon-angle-left"></i></a>
+                    <a data-slide="next" href="#myCarousel" class="right carousel-control"><i class="icon-angle-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div><!--/row-fluid-->
+    <div class="row-fluid margin-bottom-30">
+        <div class="span6">
+             <p>
 
-    <!-- Coming Soon Plugn -->
-    <div class="row-fluid">
-        <div class="coming-soon-plugin">
-            <div id="defaultCountdown"></div>
+            </p>
+        </div><!--/span6-->
+        <div class="span6">
+
         </div>
-    </div>
+    </div><!--/row-fluid-->
+
 
 </div><!--/container-->
 <!--=== End Content Part ===-->
+
 @stop
